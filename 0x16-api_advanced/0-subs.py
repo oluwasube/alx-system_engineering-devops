4a404b45a58012ce078bfada79d8c20b634600a0
oluwasube <oluwasube@outlook.com>
2022-11-02 15:28:03 +0100

    included user agent

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -6,12 +6,14 @@ import requests
 def number_of_subscribers(subreddit):
     """Return the number of subcribers for a given subreddit"""
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {
+    custom_headers = {
                "User-Agent":
                "ubuntu4.20:0x16-api:v1.0.0 (by /u/oluwasube)"
               }
     try:
-        response = requests.get(url, headers=headers).json()
+        response = requests.get(url,
+                                headers=custom_headers,
+                                allow_redirects=False).json()
         return response["data"]["subscribers"]
     except Exception:
         return 0
